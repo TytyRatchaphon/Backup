f5f3b78c49e59476d6fef546665dedbc6e7b460d
nataya-a <nataya.akpic@gmail.com>
2025-03-19 09:04:12 +0700

    page edit ver

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,25 +18,46 @@ export default function Home() {
         </nav>
       </header>
 
-      <div style={{backgroundColor: '#EAF8EE', borderRadius: '0 0 100px 100px', borderBottom: '1px solid #3EBE71', height: '500px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-        <div className="max-w-5xl mx-auto px-4 xl:px-0 pt-24 lg:pt-32 pb-24">
-          <h1 className="font-semibold text-[#3EBE71] text-5xl md:text-6xl">
-            <span className="text-black ">Preline Agency:</span> Transforming ideas into reality
-          </h1>
-          <div className="max-w-4xl">
-            <p className="mt-5 text-neutral-400 text-lg">
-              It is a creative hub where imagination meets craftsmanship to transform ideas into tangible realities. At Preline Agency, we specialize in turning conceptual visions into concrete forms, whether it be through design, artistry, or technological innovation.
-            </p>
+      <div className='bg-[#EAF8EE] rounded-bl-3xl rounded-br-3xl h-[400px] flex justify-between items-center'>
+        <div className="max-w-5xl mx-auto px-4 xl:px-0 pb-12">
+          <div>
+            <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-10">
+              <div className="mt-5 max-w-2xl text-center mx-auto">
+                <h1 className="block font-bold text-gray-800 text-4xl md:text-5xl lg:text-6xl">
+                  GoMed:
+                  <span className="text-[#3EBE71]">Your Trusted Online Pharmacy</span>
+                </h1>
+              </div>
+
+              <div className="mt-5 max-w-3xl text-center mx-auto">
+                <p className="text-lg text-gray-600">Effortlessly connect with quality pharmacies in your area. Whether you need expert advice from a pharmacist or the right medication for your needs, we're here for you anytime.</p>
+                <p className='text-lg text-gray-600'>With comprehensive information and attentive care, your health is our priority.</p>
+              </div>
+
+            </div>
           </div>
         </div>
       </div>
+
+      {/* <div style={{backgroundColor: '#EAF8EE', borderRadius: '0 0 100px 100px', borderBottom: '1px solid #3EBE71', height: '400px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <div className="max-w-5xl mx-auto px-4 xl:px-0 pt-24 lg:pt-32 pb-24">
+          <h1 className="font-semibold text-[#3EBE71] text-5xl md:text-6xl">
+            <span className="text-black ">GoMed: </span> Your Trusted Online Pharmacy
+          </h1>
+          <div className="max-w-4xl">
+            <p className="mt-5 text-neutral-400 text-lg">
+            Effortlessly connect with quality pharmacies in your area. Whether you need expert advice from a pharmacist or the right medication for your needs, we're here for you anytime. With comprehensive information and attentive care, your health is our priority.
+            </p>
+          </div>
+        </div>
+      </div> */}
 
       <section className="bg-white ml-10 mr-10">
         <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
           <img className="w-[1000px] h-[400px] object-cover" src="/Customer.jpg" alt=""/>
           <div className="mt-4 md:mt-0">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">Sign Up as Customer</h2>
-            <p className="mb-6 font-light text-gray-500 md:text-lg">Flowbite helps you connect with friends and communities of people who share your interests. Connecting with your friends and family as well as discovering new ones is easy with features like Groups.</p>
+            <p className="mb-6 font-light text-gray-500 md:text-lg">Easily access various pharmacies in your area. Purchase medications and consult with professional pharmacists anytime through real-time chat.</p>
             <Link className='bg-blue-600 text-white font-semibold px-10 py-1 rounded-lg' href='/customer/SignUp'>Sign Up</Link>
           </div>
         </div>
@@ -47,7 +68,7 @@ export default function Home() {
           <img className="w-[1000px] h-[400px] object-cover" src="/Pharmacist.jpg" alt=""/>
           <div className="mt-4 md:mt-0">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">Sign Up as Pharmacy</h2>
-            <p className="mb-6 font-light text-gray-500 md:text-lg">Flowbite helps you connect with friends and communities of people who share your interests. Connecting with your friends and family as well as discovering new ones is easy with features like Groups.</p>
+            <p className="mb-6 font-light text-gray-500 md:text-lg">Join our online pharmacy network to expand your reach. Provide consultations and assist customers effectively through real-time chat support.</p>
             <Link className='bg-blue-600 text-white font-semibold px-10 py-1 rounded-lg' href='/pharmacy/SignUp'>Sign Up</Link>
           </div>
         </div>
