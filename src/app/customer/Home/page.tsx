e068b310e19426ae6c04145744b29a723a474268
nataya-a <nataya.akpic@gmail.com>
2025-03-21 00:19:09 +0700

    MyProfile

--- src/app/customer/Home/page.tsx
+++ src/app/customer/Home/page.tsx
@@ -32,6 +32,9 @@ export default function Home() {
                             <Link href="/customer/Profile">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-circle-user-round"><path d="M18 20a6 6 0 0 0-12 0" className="text-[#3EBE71]"/><circle cx="12" cy="10" r="4" className="text-[#3EBE71]"/><circle cx="12" cy="12" r="10" className="text-[#3EBE71]"/></svg>
                             </Link>
+                            <Link href="/SignIn" className="text-[#3EBE71]">
+                                Log Out
+                            </Link>
                         </div>
                     </div>
                 </nav>
